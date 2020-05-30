34217a1cf8208ff109bde44912d2d2fb62768fdb
serebriakov19 <57367859+serebriakov19@users.noreply.github.com>
2020-05-30 21:59:22 +0300

    Add files via upload

--- Parceptron.java
+++ Parceptron.java
@@ -2,9 +2,13 @@ package com.example.lab3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,6 +16,7 @@ public class Parceptron extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //final Dialog pop_up;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parceptron);
         final int P = 4;
@@ -26,6 +31,9 @@ public class Parceptron extends AppCompatActivity {
         }
         points.setText(arr);
 
+        //pop_up=new Dialog(this);
+
+
         Button calc = (Button) findViewById(R.id.button);
         calc.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,22 +44,45 @@ public class Parceptron extends AppCompatActivity {
                 double sigma = Double.parseDouble(((TextView) findViewById(R.id.edSigma)).getText().toString());
                 double[] result = parceptron(P, pointsArr, sigma, t, iter);
                 String res;
+
+                /*pop_up.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                pop_up.setContentView(R.layout.pop_up);
+                pop_up.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+                Button btnOk = (Button)pop_up.findViewById(R.id.ok);
+                TextView pop_up_time = (TextView)pop_up.findViewById(R.id.pop_up_time);
+                pop_up_time.setBackground(new ColorDrawable(Color.WHITE));
+
+                btnOk.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        pop_up.dismiss();
+                    }
+                });
+                pop_up_time.setText("Час виконання: " + (int)result[3] + "наносекунд");
+                pop_up.show();*/
+
+
                 res = "w1=" + result[0] + "\n";
                 res += "w2=" + result[1] + "\n";
-                res += "Count of iteration: " + result[2]+"\n";
-                res += "Time: " + result[3]+"\n";
+                res += "Кількість ітерацій: " + result[2]+"\n";
+                res += "Час виканання: " + (int)result[3]+"\n";
+
                 ((TextView) findViewById(R.id.res)).setText(res);
             }
         });
+
+
+
     }
 
     public static double[] parceptron(int P, int[][] points, double sigma, long time, int iter) {
         double delta;
         double y;
         double w1 = 0, w2 = 0;
+        time*=1000000;
 
         int j = 0;
-        long timeBefore = System.currentTimeMillis();
+        long timeBefore = System.nanoTime();
         long timeAfter = 0;
         for (int k = 0; k < iter; k++) {
             {
@@ -64,7 +95,7 @@ public class Parceptron extends AppCompatActivity {
                     delta = P - y;
                     w1 = w1 + delta * sigma * points[i][0];
                     w2 = w2 + delta * sigma * points[i][1];
-                    timeAfter = System.currentTimeMillis();
+                    timeAfter = System.nanoTime();
                     if (w1 == Double.NEGATIVE_INFINITY) {
                         System.out.println();
                     }
@@ -76,14 +107,14 @@ public class Parceptron extends AppCompatActivity {
                             w1 * points[2][0] + w2 * points[2][1] < P &&
                             w1 * points[3][0] + w2 * points[3][1] < P) ||
                             timeAfter - timeBefore > time) {
-                        double[] arr = {w1, w2, j, timeAfter - timeBefore};
+                        double[] arr = {w1, w2, j, (timeAfter - timeBefore)};
                         return arr;
 
                     }
                 }
             }
         }
-        double[] arr = {w1, w2, j, timeAfter - timeBefore};
+        double[] arr = {w1, w2, j, (timeAfter - timeBefore)};
         return arr;
     }
 
